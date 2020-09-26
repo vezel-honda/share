c9de36a53ef3c69f3a3521f00747f80389e12498
vezel-honda <fleur.soleil.15@gmail.com>
2020-09-26 22:22:41 +0900

    20200926-1-commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,36 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
+import SignUp from '../views/SignUp.vue';
+
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
   }
-]
+];
+
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
 export default router
---
+++ src/views/SignUp.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <headerAuth />
+    <div class="card">
+      <p>新規登録</p>
+      <div class="form">
+        <input placeholder="ユーザーネーム" type="text" v-model="name" />
+        <input placeholder="プロフィール" type="text" v-model="profile" />
+        <input placeholder="メールアドレス" type="email" v-model="email" />
+        <input placeholder="パスワード" type="password" v-model="password" />
+        <button @click="addAuth">新規登録</button>
+      </div>  
+    </div>  
+  </div>  
+  
+</template>
+
+<script>
+import HeaderAuth from "../components/HeaderAuth";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      name: "",
+      profile: "",
+      email: "",
+      password: ""
+    };
+  },
+  components: {
+    HeaderAuth
+  },
+  methods: {
+    addAuth() {
+      axios
+        .post("https://infinite-shelf-65904.herokuapp.com/api/register", {
+          name: this.name,
+          profile: this.profile,
+          email: this.email,
+          password: this.password
+        })
+        .then(response => {
+          console.log(response);
+          this.$router.replace("/");
+        })
+        .catch(error => {
+          alert(error);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  width: 100px;
+  text-align: center;
+  padding: 8px 0 10px;
+  color: #fff;
+  background-color: #5419da;
+  border-radius: 25px;
+  cursor: pointer;
+}
+.card {
+  margin: 100px auto;
+  width: 350px;
+  background: #fff;
+  border-radius: 5px;
+  padding: 20px;
+}
+.card p {
+  color: black;
+  font-weight: bold;
+  text-align: center;
+}
+input {
+  margin-top: 15px;
+  width: 80%;
+  border-radius: 10px;
+  padding: 10px;
+  border: 1px solid black;
+  color: black;
+}
+.form {
+  text-align: center;
+}
+.form button {
+  margin-top: 15px;
+}
+</style>
